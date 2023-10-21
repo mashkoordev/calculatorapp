21454f0ee2481a4e5576930023b635371f733341
m mashkoor <mashkoorkhan014@gmail.com>
2023-10-21 09:17:18 +0500

    how to create button

--- lib/home.dart
+++ lib/home.dart
@@ -1,16 +1,10 @@
-import 'package:badges/badges.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:pin_code_fields/pin_code_fields.dart';
-import 'package:readmore/readmore.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:studioproject2/constant.dart';
+
+
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-    systemNavigationBarColor: Colors.transparent
-  ));
+
   runApp( HomeView());
 }
 class HomeView extends StatefulWidget {
@@ -26,43 +20,46 @@ class _HomeViewState extends State<HomeView> {
     return Scaffold(
       appBar:
         AppBar(
-          leading: CircleAvatar(
-            backgroundImage: AssetImage("assets/mashkoor.jpg"),
-          ),
-          title: Text("my app",style: TextStyle(fontSize: 18,fontFamily: "Pacifico"),),
+          title:
+          Text("Calculator",style: TextStyle(fontSize: 24,fontFamily: "Pacifico"),),
+
       ),
-      body:
-         Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-            PinCodeTextField(
-                appContext: context,
-                length: 6,
-                keyboardType: TextInputType.number,
-              cursorColor: Colors.teal,
-              obscureText: true,
-              enabled: true,
-              pinTheme: PinTheme(
-                shape: PinCodeFieldShape.box,
-                borderRadius: BorderRadius.circular(10),
-                fieldWidth: 40,
-                fieldHeight: 50,
-                activeColor: Colors.grey,
-                inactiveFillColor: Colors.grey,
-                selectedColor: Colors.pink,
-                inactiveColor: Colors.grey,
-                selectedFillColor: Colors.purple.shade500,
-                activeFillColor: Colors.red,
-              ),
-              onChanged: (value){
-
-              },
-            ),
-
-
-            ],
-          ),
+    body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          children: [
+           MyButton(
+          title: "log in",),
+            MyButton(
+              title: "sign up",),
+          ],
+        ),
+      ),
+    ),
     );
   }
 }
+
+
+
+class MyButton extends StatelessWidget {
+  final String title;
+  const MyButton({super.key,required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Container(
+        height: 50,
+        decoration: BoxDecoration(
+          color: Colors.deepOrange,
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: Center(child: Text(title,style: TextStyle(color: Colors.white),)),
+      ),
+    );
+  }
+}
+
